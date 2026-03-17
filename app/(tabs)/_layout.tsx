223a04e002fbc0072baf4b6a6e954d7df4abf969
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2026-03-17 18:42:57 +0100

    Create tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,19 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+    return (
+        <Tabs 
+            screenOptions={{ 
+                tabBarActiveTintColor: 'black', 
+                headerShown: false,
+            }}
+        >
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'home',
+                }}
+            />
+        </Tabs>
+    );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,16 @@
 import "../global.css";
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <ThemedView className="flex-1">
-      <StatusBar style="auto" />
-      <SafeAreaView className="flex-1">
-        <ThemedView className="flex-1 items-center justify-center">
-          <ThemedText className="font-bold">Hello world!</ThemedText>
-        </ThemedView>
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1" edges={['top']}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)"/>
+        </Stack>
       </SafeAreaView>
-    </ThemedView>
+    </SafeAreaProvider>
   );
 }
